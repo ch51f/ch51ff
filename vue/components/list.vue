764996f44c1754df581c3714efdd5f5ebdee8726
ch51ff <371476909@qq.com>
2017-03-23 17:37:09 +0800

    ...

--- vue/components/list.vue
+++ vue/components/list.vue
@@ -1,6 +1,57 @@
 <template>
-	<div>我是list</div>
+	<div>
+		<div v-bind:title="message">{{msg}}</div>
+		<div v-if="seen">Now you see me</div>
+		<ol>
+			<li v-for="todo in todos">{{todo.text}}</li>
+			<todo v-for="item in todos" v-bind:todo="item"></todo>
+		</ol>
+		<button v-on:click='reverseMessage'>Reverse Message</button>
+		<div>{{message}}</div>
+		<div v-once>{{message}}</div>
+		<div><input type="text" v-model="message"></div>
+		<div v-html="html"></div>
+		<div>
+			<p>Originnal message: "{{msg}}"</p>
+			<p>Computed reversed message: "{{reverseMsg}}"</p>
+		</div>
+		<div>
+			<button @click="counter += 1">add counter</button>
+			<p>Counter: {{counter}}</p> 
+		</div>
+	</div>
 </template>
 
 <script>
+import todo from './todo';
+export default {
+	name: 'demo',
+	data () {
+		return {
+			msg: 'hello word 123',
+			message: 'hi, this is title',
+			seen: true,
+			todos: [
+				{text: 'Learn JavaScript'},
+				{text: 'Learn Vue'},
+				{text: 'Build something awesome'}
+			],
+			html: '<p>html</p>',
+			counter: 0,
+		}
+	},
+	methods: {
+		reverseMessage() {
+			this.msg = this.msg.split(' ').reverse().join(' ')
+		}
+	},
+	computed: {
+		reverseMsg() {
+			return this.msg.split(' ').reverse().join(' ')
+		}
+	},
+	components: {
+		todo,
+	}
+}
 </script>
---
+++ vue/components/todo.vue
@@ -0,0 +1,10 @@
+<template>
+	<div>{{todo}}</div>
+</template>
+
+<script>
+export default {
+	name: 'todo',
+	props: ['todo']
+}
+</script>
