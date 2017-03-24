f9e7dbcd604aba499e0d9b5cfe91c13a2c64c1fb
ch51ff <371476909@qq.com>
2017-03-24 16:26:33 +0800

    ...

--- src/babel.js
+++ src/babel.js
@@ -245,3 +245,154 @@ two.next = three;
 for(var i of one) {
 	console.log(i);
 }*/
+
+/*var obj = new Proxy({}, {
+	get: function(target, key, receiver) {
+		console.log(`getting ${key}`);
+		return Reflect.get(target, key, receiver);
+	},
+	set: function(target, key, value, receiver) {
+		console.log(`setting ${key}`);
+		return Reflect.set(target, key, value, receiver);
+	}
+});
+
+obj.count =  1;
+
+++obj.count;*/
+
+/*var proxy = new Proxy({}, {
+	get: function(target, property) {
+		return 35;
+	}
+});
+
+console.log(proxy.time)
+console.log(proxy.name)
+console.log(proxy.title)*/
+
+/*var proxy = new Proxy({}, {
+	get: function(target, property) {
+		return 35;
+	}
+});
+
+let obj = Object.create(proxy);
+
+console.log(obj.time)*/
+
+/*var handler = {
+	get: function(target, name) {
+		if(name === 'prototype') {
+			return Object.prototype;
+		}
+		return 'Hello, ' + name;
+	},
+
+	apply: function(target, thisBinding, args) {
+		return args[0];
+	},
+
+	construct: function(target, args) {
+		return {value: args[1]};
+	}
+}
+
+var fproxy = new Proxy(function(x, y) {
+	return x + y;
+}, handler);
+
+console.log(fproxy(1, 2))
+console.log(new fproxy(1, 2))
+console.log(fproxy.prototype === Object.prototype)
+console.log(fproxy.foo)*/
+
+/*var person = {
+	name: "张三"
+};
+
+var proxy = new Proxy(person, {
+	get: function(target, property) {
+		if(property in target) {
+			return target[property];
+		} else {
+			throw new ReferenceError("Property \"" + property + "\" dose not exist.");
+		}
+	}
+})
+
+console.log(proxy.name)
+console.log(proxy.age)*/
+
+/*let proto = new Proxy({}, {
+	get(target, propetyKey, receiver) {
+		console.log('GET ' + propetyKey);
+		return target[propetyKey];
+	}
+})
+
+let obj = Object.create(proto);
+obj.xxx*/
+
+/*function createArray(...elements) {
+	let handler = {
+		get(target, propKey, receiver) {
+			let index = Number(propKey);
+			if(index < 0) {
+				propKey = String(target.length + index);
+			}
+			return Reflect.get(target, propKey, receiver);
+		}
+	};
+
+	let target = [];
+	target.push(...elements);
+	return new Proxy(target, handler);
+}
+
+let arr = createArray('a', 'b', 'c');
+console.log(arr[-1])*/
+
+var pipe = (function() {
+	return function(value) {
+		var funcStack = [];
+		var oproxy = new Proxy({}, {
+			get: function(pipeObject, fnName) {
+				if(fnName === 'get') {
+					return funcStack.reduce(function(val, fn) {
+						return fn(val)
+					}, value);
+				}
+				funcStack.push(window[fnName]);
+				return oproxy;
+			}
+		});
+		return oproxy;
+	}
+}());
+
+var double = n => n * 2;
+var pow = n => n * n;
+var reverseInt = n => n.toString().split("").reverse().join("") | 0;
+
+pipe(3).double.pow.reverseInt.get;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
