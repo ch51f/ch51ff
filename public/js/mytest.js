b9370cce89e843da96f8e47fd104d84ccd23aea6
ch51ff <371476909@qq.com>
2018-07-13 17:59:51 +0800

    ...

--- public/js/mytest.js
+++ public/js/mytest.js
@@ -1,196 +1,10 @@
-// function Point(x, y) {
-//   this.x = x;
-//   this.y = y;
-// }
-
-// Point.prototype.toString = function() {
-//   return '(' + this.x + ', ' + this.y + ')';
-// };
-
-// var p = new Point (1, 2);
-
-// // console.log(p.toString());
-
-// class newPoint {
-//   constructor(x, y) {
-//     this.x = x;
-//     this.y = y;
-//   }
-//   toString() {
-//     return `(${this.x}, ${this.y})`;
-//   }
-// }
-
-// var p1 = new newPoint(5, 4);
-// // console.log(p1.toString());
-
-// function* hwGenerator() {
-//   yield 'hello';
-//   yield 'world';
-//   return 'ending';
-// }
-
-// var hw = hwGenerator();
-
-// // console.log(hw.next());
-// // console.log(hw.next());
-// // console.log(hw.next());
-// // console.log(hw.next());
-// // console.log(hw.next());
-
-// var arr = [1, [[2, 3], 4], [5, 6]];
-
-// var flat = function* (a) {
-//   // a.forEach(function(item) {
-//   //   if(typeof item !== 'number') {
-//   //     yield* flat(item);
-//   //   } else {
-//   //     yield 'item';
-//   //   }
-//   // })
-//   var length = a.length;
-//   for(var i = 0; i < length; i++) {
-//     var item = a[i];
-//     if(typeof item !== 'number') {
-//       yield* flat(item);
-//     } else {
-//       yield item;
-//     }
-//   }
-// }
-
-// // for(var f of flat(arr)) {
-// //   console.log(f);
-// // }
-
-// var myIterable = {};
-// myIterable[Symbol.iterator] = function* () {
-//   yield 1;
-//   yield 2;
-//   yield 3;
-// };
-
-// // console.log([...myIterable])
-
-// function* f() {
-//   for(var i = 0; true; i++) {
-//     var reset = yield i;
-//     if(reset) { i = -2;}
-//   }
-// }
-
-// var g = f();
-
-// // console.log(g.next())
-// // console.log(g.next())
-// // console.log(g.next())
-// // console.log(g.next(true))
-// // console.log(g.next())
-
-// function* foo(x) {
-//   var y = 2 * (yield(x + 1));
-//   var z = yield(y / 3);
-//   return (x + y + z)
-// }
-
-// var a = foo(5);
-// // console.log(a.next())
-// // console.log(a.next())
-// // console.log(a.next())
-
-// var b = foo(5);
-// // console.log(b.next())
-// // console.log(b.next(12))
-// // console.log(b.next(13))
-
-// function *foo1() {
-//   for(var i =0; i < 7; i++) {
-//     yield i;
-//   }
-//   return 7;
-// }
-
-// for(let v of foo1()) {
-//   // console.log(v);
-// }
-
-// function* fibonacci() {
-//   let [prev, curr] = [0, 1]
-//   for(;;) {
-//     [prev, curr] = [curr, prev + curr];
-//     yield curr;
-//   }
-// }
-
-// // for(let n of fibonacci()) {
-// //   if(n > 1000) break;
-// //   console.log(n);
-// // }
-
-// function* objectEntries(obj) {
-//   let propKeys = Reflect.ownKeys(obj);
-
-//   for(let propKey of propKeys) {
-//     yield [propKey, obj[propKey]];
-//   }
-// }
-
-// let jane = {first: 'Jane', last: 'Doe'};
-
-// // for(let [key, value] of objectEntries(jane)) {
-// //   console.log(`${key}: ${value}`);
-// // }
-
-// let s1 = Symbol('foo');
-// let s2 = Symbol('bar');
-
-// console.log(s1)
-// console.log(s2)
-
-// console.log(s1.toString())
-// console.log(s2.toString())
-
-// const obj = {
-//   toString() {
-//     return 'abc';
-//   }
-// };
-// const sym = Symbol(obj);
-
-// console.log(sym)
-
-// let mySymbol = Symbol();
-
-// let a = {};
-// a[mySymbol] = 'Hello!';
-
-// console.log(a[mySymbol]);
-
-// var g = function* () {
-//   try {
-//     yield;
-//   } catch(e) {
-//     console.log('内部错误', e);
-//   }
-// };
-
-// var i = g();
-// i.next();
-
-// try {
-//   i.throw('a');
-//   i.throw('b');
-// } catch(e) {
-//   console.log('外部捕获', e);
-// }
-
 // function timeout(ms) {
 //   return new Promise((resolve, reject) => {
 //     setTimeout(resolve, ms, 'done');
-//   });
+//   })
 // }
 
-// timeout(1000).then((value) => {
+// timeout(100).then((value) => {
 //   console.log(value);
 // })
 
@@ -200,7 +14,7 @@
 // });
 
 // promise.then(function() {
-//   console.log('resolved.');
+//   console.log('resolved.')
 // });
 
 // console.log('Hi!');
@@ -217,8 +31,62 @@
 //       reject(new Error('Could not load image at ' + url));
 //     };
 
+
 //     image.src = url;
-//   })
+//   });
 // }
 
-//  
+// var getJSON = function(url) {
+//   var promise = new Promise(function(resolve, reject) {
+//     var client = new XMLHttpRequest();
+//     client.open("GET", url);
+//     client.onreadystatechange = handler;
+//     client.responseType = 'json';
+//     client.setRequestHeader("Accept", "application/json");
+//     client.send();
+
+//     function handler() {
+//       if(this.readyState !== 4) {
+//         return;
+//       }
+//       if(this.status === 200) {
+//         console.log(this)
+//         resolve(this.response);
+//       } else {
+//         reject(new Error(this.statusText));
+//       }
+//     }
+//   });
+//   return promise;
+// };
+
+// getJSON("/asset/posts.json").then(function(json) {
+//   console.log('Contents: ' + json);
+// }, function(error) {
+//   console.error('出错了', error);
+// });
+
+// var p1 = new Promise(function (resolve, reject) {
+//   setTimeout(() => reject(new Error('fail')), 3000)
+// })
+
+// var p2 = new Promise(function (resolve, reject) {
+//   setTimeout(() => resolve(p1), 1000)
+// })
+
+// p2.then(result => {
+//   console.log(result)
+//   console.log(1)
+// }).catch(error => console.log(error))
+
+let birth = '1989/05/01';
+
+const Person = {
+  name: 'fch',
+  birth,
+  hello() {
+    console.log('name', this.name, 'birth', this.birth)
+  }
+}
+
+Person.hello();
